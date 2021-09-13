f2719befe2c6334a66cc403c53794d7b6959748a
Jake Park <qkstjr09@gmail.com>
2021-09-13 17:49:23 +0900

    Feat: Add Nav Components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Nav />
   <router-view/>
 </template>
+
+<script>
+import Nav from '@/components/Nav.vue';
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
 
 <style lang="scss" src="./assets/css/style.scss" />
---
+++ src/components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+<nav class="nav">
+  <ul class="nav-list">
+    <li>
+      <button @click="today">
+        <i class="xi-home" />
+        <span>오늘</span>
+      </button>
+    </li>
+    <li>
+      <router-link to="/calendar">
+        <i class="xi-calendar" />
+        <span>달력</span>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/setting">
+        <i class="xi-cog" />
+        <span>설정</span>
+      </router-link>
+    </li>
+    <li>
+      <router-link to="/about">
+        <i class="xi-emoticon-smiley" />
+        <span>소개</span>
+      </router-link>
+    </li>
+  </ul>
+</nav>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Nav',
+  methods: {
+    today() {
+      const todayUrl = `/day/${moment().format('YYYY-MM-DD')}`;
+      console.log(todayUrl);
+      this.$router.push(todayUrl);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
